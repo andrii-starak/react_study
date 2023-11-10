5320b3c9693c04208815320fb1f9cd2ba93232ab
Andrii Starak <andrii.starak6@gmail.com>
2023-11-10 11:24:16 +0200

    Creating components, nesting components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 import React from 'react'
-// import { createRoot, hydrateRoot } from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 
-function Greeting() {
-  return <h2>My First Component</h2>
+const BookList = () => {
+  return (
+    <section>
+      <Book></Book>
+      <Book></Book>
+      <Book></Book>
+      <Book></Book>
+    </section>
+  )
 }
 
+const Book = () => (
+  <article>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+)
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/61BWsc9eGbL._AC_UL600_SR600,400_.jpg"
+      alt="The Woman in Me"
+    ></img>
+  )
+}
+const Title = () => {
+  return <h2>The Woman in Me</h2>
+}
+const Author = () => {
+  return <h4>Britney Spears</h4>
+}
 const root = createRoot(document.getElementById('root'))
 
-root.render(<Greeting></Greeting>)
+root.render(<BookList></BookList>)
