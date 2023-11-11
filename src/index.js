15b7fe2ba5b793fe2de5ea4c48269520fa5fad9b
Andrii Starak <andrii.starak6@gmail.com>
2023-11-11 18:39:19 +0200

    css in jsx

--- src/index.js
+++ src/index.js
@@ -29,7 +29,12 @@ const Title = () => {
   return <h2>The Woman in Me</h2>
 }
 const Author = () => {
-  return <h4>Britney Spears</h4>
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  }
+  return <h4 style={inlineHeadingStyles}>Britney Spears</h4>
 }
 const root = createRoot(document.getElementById('root'))
 
