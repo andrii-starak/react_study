f94e86b2a7cf4468f060fcdd32462842c629988f
Andrii Starak <andrii.starak6@gmail.com>
2023-11-12 21:22:11 +0200

    Children Prop

--- src/index.js
+++ src/index.js
@@ -3,38 +3,29 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 
-const firstBook = {
-  author: 'Jordan Moore',
-  title: 'Interesting Facts For Curious Minds',
-  img: './images/book-1.jpg',
-}
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-}
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: './images/book-1.jpg',
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  },
+]
+
 // const author = 'Jordan Moore'
 // const title = 'Interesting Facts For Curious Minds'
 // const img = './images/book-1.jpg'
 
 const BookList = () => {
-  return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      ></Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      ></Book>
-    </section>
-  )
+  return <section className="booklist">{books}</section>
 }
 const Book = (props) => {
   const { img, title, author } = props
+  console.log(props)
   return (
     <article className="book">
       <img src={img} alt={title} />
